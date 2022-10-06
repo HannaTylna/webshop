1fd00e5ade4ccf6238fb510e3b6db768557d053d
Kriksy <viktoria.toldi@gmail.com>
2022-10-06 14:10:52 +0200

    Update signUpUser to use createUser

--- packages/backend/src/controllers/user.ts
+++ packages/backend/src/controllers/user.ts
@@ -1,9 +1,16 @@
 import express, { Router, Request, Response } from "express"
+import { createUser } from "../models/users"
+import { User } from "@webshop/shared"
 
 const userController: Router = express.Router()
+interface CustomRequest<T> extends Request {
+  body: T
+}
 
-export function signUpUser(req: Request, res: Response) {
-  res.send({ message: "Sign up user" })
+export async function signUpUser(req: CustomRequest<User>, res: Response) {
+  const body = req.body as User
+  let user: User = await createUser(body)
+  res.send({ user: user })
 }
 
 // Sign Up User localhost:4000/user/signUp
