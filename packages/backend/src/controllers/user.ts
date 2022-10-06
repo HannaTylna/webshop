856d40c424a3b6288456d22a314612fd5b7b27d0
Kriksy <viktoria.toldi@gmail.com>
2022-10-06 18:56:39 +0200

    Add try catch to signUpUser

--- packages/backend/src/controllers/user.ts
+++ packages/backend/src/controllers/user.ts
@@ -9,8 +9,14 @@ interface CustomRequest<T> extends Request {
 
 export async function signUpUser(req: CustomRequest<User>, res: Response) {
   const body = req.body as User
-  let user: User = await createUser(body)
-  res.send({ user: user })
+  try {
+    let user: User = await createUser(body)
+    res.send({ user: user })
+  } catch (error) {
+    res
+      .sendStatus(400)
+      .send({ message: "failed to sign up user", error: error })
+  }
 }
 
 // Sign Up User localhost:4000/user/signUp
