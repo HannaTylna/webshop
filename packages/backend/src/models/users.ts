7354d3b2433a9ea75cc7781d0445990902d240d7
Kriksy <viktoria.toldi@gmail.com>
2022-10-06 14:09:37 +0200

    Add createUser to models/users

--- packages/backend/src/models/users.ts
+++ packages/backend/src/models/users.ts
@@ -14,3 +14,9 @@ const UserModel = model<User>("User", userSchema)
 export const getUser = async () => {
   return UserModel.find({}).exec()
 }
+
+export const createUser = async (user: User): Promise<User> => {
+  const u = new UserModel(user)
+  u.save()
+  return u
+}
