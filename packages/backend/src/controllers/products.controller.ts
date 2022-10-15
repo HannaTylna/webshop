54efa20de72811659c500de340f0dc2033bdc38b
MadSamson <m.mahdi.samadi@gmail.com>
2022-10-15 15:46:47 +0200

    error handling

--- packages/backend/src/controllers/products.controller.ts
+++ packages/backend/src/controllers/products.controller.ts
@@ -4,15 +4,20 @@ import {getAllProducts, getProduct} from '../models/products'
 const productsController = express.Router()
 
 productsController.get('/', async(req: Request, res: Response)=>{
-    res.send(await getAllProducts())
+    try {
+        res.status(200).json(await getAllProducts())
+    } catch (error) {
+        res.status(400).json(error)
+    }
+    
 })
 
 productsController.get('/:id', async(req: Request, res: Response)=>{
     try {
         const productId = req.params.id
-        res.send(await getProduct(productId))
+        res.status(200).json(await getProduct(productId))
     } catch (error) {
-        res.send(error)
+        res.status(400).send(error)
     }
 })
 
