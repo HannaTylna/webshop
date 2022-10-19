92fc4e1baf4f063a6f99a991b1e1616d181c1883
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-19 19:02:12 +0200

    move auth function to index.ts

--- packages/backend/src/routers/user.ts
+++ packages/backend/src/routers/user.ts
@@ -1,11 +1,9 @@
 import express, { Router } from "express"
 import { getUserInfo, updateUserInfo } from "../controllers/auth"
-import { authenticateJwtTokenMiddleware } from "../middleware/auth"
 
 const userRouter: Router = express.Router()
 
 userRouter.get("/", getUserInfo)
-userRouter.patch("/:id", updateUserInfo)
-userRouter.use("/", authenticateJwtTokenMiddleware)
+userRouter.patch("/", updateUserInfo)
 
 export default userRouter
