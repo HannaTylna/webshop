3f0954fd016e86969bf8a8846ec697519b0a2834
yihu55 <veronicayi@msn.com>
2022-10-20 11:13:46 +0200

    search function implemented

--- packages/backend/src/routers/products.routes.ts
+++ packages/backend/src/routers/products.routes.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express"
-import {loadAllProducts, loadAProduct} from "../controllers/products.controller"
+import {
+  loadAllProducts,
+  loadAProduct,
+  searchProduct,
+} from "../controllers/products.controller"
 
 const productsRouter: Router = express.Router()
 
 productsRouter.get("/", loadAllProducts)
 productsRouter.get("/:id", loadAProduct)
+productsRouter.get("/search/:key", searchProduct)
 
 export default productsRouter
