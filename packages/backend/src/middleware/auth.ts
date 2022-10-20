0f5d727964469566f3f505d42342078591ee0b93
Kriksy <viktoria.toldi@gmail.com>
2022-10-20 14:49:07 +0200

    Test saving token in tokenlist

--- packages/backend/src/middleware/auth.ts
+++ packages/backend/src/middleware/auth.ts
@@ -3,14 +3,24 @@ import { Request, Response, NextFunction } from "express"
 
 const JWT_SECRET: string = process.env.JWT_TOKEN || "your_jwt_secret"
 
+const tokenList = new Map<string, JwtPayload>()
+
 export type JwtPayload = {
   username: string | undefined
 }
 export interface JwtRequest<T> extends Request<T> {
   jwt?: JwtPayload
+  refreshToken?: JwtPayload
 }
 
 export function createJwtToken(payload: JwtPayload) {
+  const token: string = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" })
+  tokenList.set(token, payload)
+  console.log(tokenList)
+  return token
+}
+
+export function createRefreshToken(payload: JwtPayload) {
   const token: string = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" })
   return token
 }
@@ -20,12 +30,20 @@ export function authenticateJwtTokenMiddleware(
   res: Response,
   next: NextFunction
 ) {
+  console.log(tokenList)
+
   const authHeader: string | undefined = req.header("Authorization")
   if (authHeader) {
     const token = authHeader.split(" ")[1]
     if (token) {
+      if (tokenList.has(token)) {
+        console.log("Token exists in tokenList")
+      } else {
+        return res.sendStatus(400) // bad token
+      }
       const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload
       req.jwt = decoded
+      console.log(decoded)
     } else {
       return res.sendStatus(400) // bad token
     }
