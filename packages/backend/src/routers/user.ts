472cf0f6869a423eb5fc43bb745a62c9b17fd49b
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-20 09:51:09 +0200

    delete

--- packages/backend/src/routers/user.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express"
-import { getUserInfo, updateUserInfo } from "../controllers/auth"
-
-const userRouter: Router = express.Router()
-
-userRouter.get("/", getUserInfo)
-userRouter.patch("/", updateUserInfo)
-
-export default userRouter
