0fc3248e4bcff1a748183b7b578b831bd2cccb03
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-22 16:14:55 +0200

    change mongodb connection

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,24 +1,18 @@
 import cors from "cors"
-import express, { Application, json, Request, Response } from "express"
-import mongoose from "mongoose"
-import { config } from "./config/db"
+import express, { Application, json } from "express"
+import { setUpMongoDb } from "./models/common"
 import router from "./routers/index"
 
 const app: Application = express()
 app.use(cors())
 app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || "4000")
+const mongoUrl: string =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/webshop"
 
 app.use("/api", router)
 
-app.listen(config.server.port, async function () {
-  await mongoose
-    .connect(config.mongo.url)
-    .then(() => {
-      console.log(`database connected`)
-    })
-    .catch((err) => {
-      console.log(`database fail to connect`, err)
-    })
-  console.log(`App is listening on port ${config.server.port} !`)
+app.listen(port, async function () {
+  await setUpMongoDb(mongoUrl)
+  console.log(`App is listening on port ${port} !`)
 })
