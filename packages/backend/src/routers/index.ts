16f657932e9fca9f06609b9d30869ab4dbcc2203
natalia-se <flaunder1000@gmail.com>
2022-10-25 09:53:24 +0200

    Add routes to the index file

--- packages/backend/src/routers/index.ts
+++ packages/backend/src/routers/index.ts
@@ -1,12 +1,12 @@
 import express from "express"
 import usersRouter from "./users"
-import productsRouter from "./products.routes";
+import productsRouter from "./products.routes"
+import ordersRouter from "./order"
 
 const router = express.Router()
 
 router.use("/user", usersRouter)
 router.use("/products", productsRouter)
+router.use("/orders", ordersRouter)
 
 export default router
-
-
