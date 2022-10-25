9d91c597ff545ee847401d6b50117ec835fbfe16
natalia-se <flaunder1000@gmail.com>
2022-10-25 20:21:50 +0200

    show orders only for authorized users

--- packages/backend/src/routers/order.ts
+++ packages/backend/src/routers/order.ts
@@ -1,15 +1,18 @@
 import express, { Router } from "express"
 import { loadAll, submit, saveCart } from "../controllers/order"
+import { authenticateJwtTokenMiddleware } from "../middleware/auth"
 
 const ordersRouter: Router = express.Router()
 
 // Fetch all orders
+ordersRouter.use("/", authenticateJwtTokenMiddleware)
 ordersRouter.get("/", loadAll)
 
 // Submit order
 ordersRouter.post("/", submit)
 
 // Save cart to orders
+ordersRouter.use("/cart", authenticateJwtTokenMiddleware)
 ordersRouter.post("/cart", saveCart)
 ordersRouter.patch("/cart", saveCart)
 
