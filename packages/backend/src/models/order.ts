0e74d79922705cbec4d13f72fea48e92182e745e
natalia-se <flaunder1000@gmail.com>
2022-10-25 09:46:41 +0200

    Add virtuals and timestamps

--- packages/backend/src/models/order.ts
+++ packages/backend/src/models/order.ts
@@ -1,14 +1,35 @@
 import { model, Schema } from "mongoose"
 import { Order } from "@webshop/shared"
 
-const orderSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  products: [],
-  shippingCost: { type: Number },
-  // totalCost: { type: Number },
-  deliveryAddress: { type: String },
-  status: { type: String, required: true },
-})
+const orderSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    products: [],
+    shippingCost: { type: Number },
+    // totalCost: { type: Number },
+    deliveryAddress: { type: String },
+    status: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    virtuals: {
+      totalCost: {
+        get() {
+          return this.products.reduce(
+            (sum, current) => sum + current.quantity * current.price,
+            0
+          )
+        },
+      },
+    },
+    toObject: {
+      virtuals: true,
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+)
 
 export const OrderModel = model<Order>("Order", orderSchema)
 
