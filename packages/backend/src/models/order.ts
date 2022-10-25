6c5b6e29ad3ed0bbbee9a5f7b067f5ea7ea9ca8b
natalia-se <flaunder1000@gmail.com>
2022-10-25 09:48:10 +0200

    Create schema for productItem

--- packages/backend/src/models/order.ts
+++ packages/backend/src/models/order.ts
@@ -1,12 +1,37 @@
 import { model, Schema } from "mongoose"
 import { Order } from "@webshop/shared"
 
+const ItemSchema = new Schema(
+  {
+    productId: { type: Schema.Types.ObjectId, ref: "Product" },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity can not be less then 1."],
+    },
+    price: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+    _id: false,
+    virtuals: {
+      total: {
+        get() {
+          return this.quantity * this.price
+        },
+      },
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+)
+
 const orderSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User" },
-    products: [],
+    products: [ItemSchema],
     shippingCost: { type: Number },
-    // totalCost: { type: Number },
     deliveryAddress: { type: String },
     status: { type: String, required: true },
   },
