163412659cedb9c86d375e48016b43997b040256
natalia-se <flaunder1000@gmail.com>
2022-10-25 09:51:33 +0200

    Add saveCart controller

--- packages/backend/src/controllers/order.ts
+++ packages/backend/src/controllers/order.ts
@@ -26,3 +26,44 @@ export const submit = async (req: Request, res: Response) => {
     res.status(400).send({ message: "failed to submit order", error: error })
   }
 }
+
+export const saveCart = async (req: Request, res: Response) => {
+  const items: Array<OrderItem> = req.body
+  const userId: string = "6356eb308324b32ccb35ea8f"
+  try {
+    let savedCart: Order | null
+    const cart = (await OrderModel.findOne({
+      user: userId,
+      status: "cart",
+    }).exec()) as Order
+
+    console.info(`order: ${cart}`)
+
+    if (cart) {
+      // console.log(cart)
+      //if _id is populated
+      cart.products = items
+      savedCart = await OrderModel.findOneAndUpdate({ _id: cart._id }, cart, {
+        new: true,
+      })
+    } else {
+      const newCart: Order = {
+        user: userId,
+        products: items,
+        totalCost: items.reduce(
+          (sum, current) => sum + current.quantity * current.price,
+          0
+        ),
+        status: "cart",
+      }
+      savedCart = await saveOrder(newCart)
+      console.info(`savedCart1: ${savedCart}`)
+    }
+    console.info(`savedCart2: ${savedCart}`)
+    res.status(200).send(savedCart)
+  } catch (error) {
+    console.log(`error: ${error}`)
+
+    res.status(400).json({ message: "failed to submit order", error: error })
+  }
+}
