2c1fc3aa824481ce15aa6031a41ef05a54351c00
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-27 15:51:01 +0200

    move axios url to app.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import axios from "axios"
 import React from "react"
 import { Container } from "react-bootstrap"
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Homepage from "./pages/Homepage"
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_WEBSHOP_API || "http://localhost:4000"
 
 function App() {
   return (
