ff2cc8f5733fa571653028bdfb4dd3517ab94a95
MadSamson <m.mahdi.samadi@gmail.com>
2022-10-27 15:20:43 +0200

    create a route for signing in

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Homepage from "./pages/Homepage"
 import SigninPage from "./pages/SigninPage"
+import SignupPage from "./pages/SignupPage"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Container>
     </>
