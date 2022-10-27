8215fc3db8668d3e440688e9cc3112a27086004b
MadSamson <m.mahdi.samadi@gmail.com>
2022-10-27 15:01:06 +0200

    creating a rout for signin

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap"
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Homepage from "./pages/Homepage"
+import SigninPage from "./pages/SigninPage"
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/signin" element={<SigninPage />} />
         </Routes>
-        <h1>Webshop 💰</h1>
       </Container>
     </>
   )
