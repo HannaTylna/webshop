f6152b468f3f21df04b70814451a1f7f10cf2cbd
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-28 10:30:21 +0200

    add getUser function

--- packages/frontend/src/pages/UserPage.tsx
+++ packages/frontend/src/pages/UserPage.tsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import avatar from "../images/avatar.jpg"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import { User } from "@webshop/shared"
+import axios from "axios"
+
+const getUser = async (): Promise<User> => {
+  const response = await axios.get<User>("/api/info")
+  return response.data
+}
 
 export default function UserPage() {
+  const [user, setUser] = useState<User>()
+
+  useEffect(() => {
+    const token = localStorage.getItem("chatapp")
+    console.log(token)
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+    console.log(config)
+    axios.get("/api/info", config).then(console.log).catch(console.log)
+  }, [])
+
   return (
     <>
       <h1 className="text-center">User page</h1>
@@ -26,7 +45,7 @@ export default function UserPage() {
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email:</Form.Label>
-              <Form.Control type="email" />
+              <Form.Control type="email" value={user?.email} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPhone">
               <Form.Label>Phone number:</Form.Label>
