148afe3c4d080e4392c0a186504cf45a71b1a19b
natalia-se <flaunder1000@gmail.com>
2022-10-29 15:37:24 +0200

    Wrap with CartProvider

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,13 +4,14 @@ import { Container } from "react-bootstrap"
 import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Homepage from "./pages/Homepage"
+import { CartProvider } from "./context/CartContext"
 
 axios.defaults.baseURL =
   process.env.REACT_APP_WEBSHOP_API || "http://localhost:4000"
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Container>
         <Routes>
@@ -18,7 +19,7 @@ function App() {
         </Routes>
         <h1>Webshop 💰</h1>
       </Container>
-    </>
+    </CartProvider>
   )
 }
 
