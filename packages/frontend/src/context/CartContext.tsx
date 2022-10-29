f1da39032bc740d1910a9c135e3e24a056ca9845
natalia-se <flaunder1000@gmail.com>
2022-10-29 15:31:41 +0200

    Add type for context

--- packages/frontend/src/context/CartContext.tsx
+++ packages/frontend/src/context/CartContext.tsx
@@ -1,5 +1,10 @@
 import { useContext, createContext, ReactNode, useState } from "react"
 // import {OrderItem} from "@webshop/shared"
+type CartContextType = {
+  getItemQuantity: (id: string) => number
+  increaseCartQuantity: (id: string) => void
+  decreaseCartQuantity: (id: string) => void
+}
 const CartContext = createContext({} as CartContextType)
 
 export const useCart = () => {
