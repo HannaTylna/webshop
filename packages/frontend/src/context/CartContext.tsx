6fd99c24388fa5f36e278151c7f9be5838064b0c
natalia-se <flaunder1000@gmail.com>
2022-10-31 11:36:00 +0100

    add create cart function and run it in useEffect

--- packages/frontend/src/context/CartContext.tsx
+++ packages/frontend/src/context/CartContext.tsx
@@ -1,6 +1,13 @@
-import { useContext, createContext, ReactNode, useState } from "react"
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react"
 import Cart from "../components/Cart"
-// import {OrderItem} from "@webshop/shared"
+import { Order } from "@webshop/shared"
+import axios from "axios"
 
 type CartProviderProps = {
   children: ReactNode
@@ -28,7 +35,9 @@ export const useCart = () => {
 
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [cart, setCart] = useState<Order[]>([])
   const [isOpen, setIsOpen] = useState(false)
+  const [error, setError] = useState<string | undefined>()
 
   const openCart = () => setIsOpen(true)
 
@@ -38,6 +47,24 @@ export const CartProvider = ({ children }: CartProviderProps) => {
     (quantity, item) => item.quantity + quantity,
     0
   )
+
+  const createCart = async (cartItems: CartItem[]): Promise<void> => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik5hdCIsInVzZXJpZCI6IjYzNTgzM2FjNjIwZTNhM2JhM2EwN2JiMyIsImlhdCI6MTY2NzIwOTAyMywiZXhwIjoxNjY3Mjk1NDIzfQ.h2Sw3CVXXEIuFNyt0JN-lDtw2tP_r11OHjv6X32fm50"
+
+    const headers = {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+
+    try {
+      await axios.post("api/orders/cart", cartItems, headers)
+      // const response = await axios.get<Order[]>("/orders")
+      //setCart(response.data)
+    } catch (err) {
+      // setCart([])
+      setError("Something went wrong when fetching cart...")
+    }
+  }
 
   const getItemQuantity = (id: string) => {
     return cartItems.find((item) => item.id === id)?.quantity || 0
@@ -74,6 +101,17 @@ export const CartProvider = ({ children }: CartProviderProps) => {
       }
     })
   }
+
+  console.log("cartItems", cartItems)
+
+  useEffect(() => {
+    createCart(cartItems)
+    // .then(setTodos)
+    // .catch((error) => {
+    //   setTodos([])
+    //   setError("Something went wrong when fetching my todos...")
+    // })
+  }, [cartItems])
 
   return (
     <CartContext.Provider
