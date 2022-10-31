d6877f55108f573b1bd8afe6c65d4a1c5142c93f
natalia-se <flaunder1000@gmail.com>
2022-10-31 20:20:59 +0100

    Use list of products from LS for cart item

--- packages/frontend/src/components/CartItem.tsx
+++ packages/frontend/src/components/CartItem.tsx
@@ -4,15 +4,30 @@ import { useCart } from "../context/CartContext"
 
 const CartItem = ({ productId, quantity, price }: OrderItem) => {
   const { increaseCartQuantity, decreaseCartQuantity } = useCart()
+  const products: string | null = localStorage.getItem("products")
+  const parsedProducts = JSON.parse(products || "")
+  const item = parsedProducts.find((item: any) => item._id === productId)
+
+  console.log("item", item)
+
   return (
     <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
-      {/* <img
-        src={item.url}
-        style={{ width: "125px", height: "75px", objectFit: "cover" }}
-        alt={item.name}
-      /> */}
+      <img
+        src={item.images.src.small}
+        style={{ width: "125px", height: "100px", objectFit: "cover" }}
+        alt={item.title}
+      />
       <div className="me-auto">
-        <div>Item Name</div>
+        <div
+          style={{
+            width: "75px",
+            height: "75px",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {item.title}
+        </div>
         <div className="text-muted">
           {quantity} X {price}
         </div>
