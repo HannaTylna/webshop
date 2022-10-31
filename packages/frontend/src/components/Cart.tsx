657ee3f8afa1a5cc29d88bcdf699ba30172d4c87
natalia-se <flaunder1000@gmail.com>
2022-10-31 17:55:26 +0100

    Add buy button

--- packages/frontend/src/components/Cart.tsx
+++ packages/frontend/src/components/Cart.tsx
@@ -1,5 +1,5 @@
-import { Offcanvas, Stack } from "react-bootstrap"
 import { useState } from "react"
+import { Offcanvas, Stack, Button } from "react-bootstrap"
 import { useCart } from "../context/CartContext"
 import CartItem from "../components/CartItem"
 
@@ -8,7 +8,7 @@ type CartProps = {
 }
 
 const Cart = ({ isOpen }: CartProps) => {
-  const { closeCart, cartItems, cart } = useCart()
+  const { closeCart, cartItems, cart, buyProducts } = useCart()
   const savedCart = cart[0] || {}
   const [address, setAddress] = useState<string>("")
   return (
@@ -16,7 +16,6 @@ const Cart = ({ isOpen }: CartProps) => {
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
-      {cartItems && (
       {cartItems.length > 0 && (
         <Offcanvas.Body>
           <Stack gap={3}>
@@ -37,6 +36,15 @@ const Cart = ({ isOpen }: CartProps) => {
               placeholder="Enter your delivery address ..."
               onChange={(e) => setAddress(e.target.value)}
             ></input>
+            <Button
+              className="w-100"
+              onClick={() => {
+                buyProducts(address)
+                setAddress("")
+              }}
+            >
+              Buy
+            </Button>
           </Stack>
         </Offcanvas.Body>
       )}
