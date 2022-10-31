e4fc52fd79811afd3dbd3e7df1a53364c435b55e
natalia-se <flaunder1000@gmail.com>
2022-10-31 09:55:02 +0100

    Add rendering cart item

--- packages/frontend/src/components/CartItem.tsx
+++ packages/frontend/src/components/CartItem.tsx
@@ -1,7 +1,31 @@
+import { Stack, Button } from "react-bootstrap"
+
 type CartItemProps = {
   id: string
   quantity: number
 }
-const CartItem = ({ id, quantity }: CartItemProps) => {}
+const CartItem = ({ id, quantity }: CartItemProps) => {
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      {/* <img
+        src={item.url}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+        alt={item.name}
+      /> */}
+      <div className="me-auto">
+        <div className="d-flex align-items-center">
+          <div>Item Name</div>
+          <div>{quantity}</div>
+        </div>
+        <div className="text-muted">Item Price</div>
+      </div>
+      <div>Total sum</div>
+      <div>
+        <Button>-</Button>
+        <Button>+</Button>
+      </div>
+    </Stack>
+  )
+}
 
 export default CartItem
