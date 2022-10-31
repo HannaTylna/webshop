ea8cd2ee1c7bd5493b9e2dfc9521d51b244c788e
natalia-se <flaunder1000@gmail.com>
2022-10-31 09:55:42 +0100

    Add body with cart items to cart

--- packages/frontend/src/components/Cart.tsx
+++ packages/frontend/src/components/Cart.tsx
@@ -1,17 +1,25 @@
-import { Offcanvas } from "react-bootstrap"
+import { Offcanvas, Stack } from "react-bootstrap"
 import { useCart } from "../context/CartContext"
+import CartItem from "../components/CartItem"
 
 type CartProps = {
   isOpen: boolean
 }
 
 const Cart = ({ isOpen }: CartProps) => {
-  const { closeCart } = useCart()
+  const { closeCart, cartItems } = useCart()
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   )
 }
