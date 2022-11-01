bd7c581c053124ceba8f3b3684ba06f1f2a6b9b0
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-01 23:21:15 +0100

    add reload trigger

--- packages/frontend/src/components/Logout.tsx
+++ packages/frontend/src/components/Logout.tsx
@@ -3,6 +3,7 @@ import {Nav} from "react-bootstrap"
 export default function LogOut() {
     const handOnClick = async():Promise<void> => {
         await localStorage.removeItem('webshop')
+        window.location.reload()
     }
   return (
     <Nav.Link onClick={handOnClick}>
