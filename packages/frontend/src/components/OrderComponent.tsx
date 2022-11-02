2f05bcd263885e253037ec66b87534f60d2631cb
HannaTylna <hanna.tylna@gmail.com>
2022-11-02 21:09:38 +0100

    add orders fetching

--- packages/frontend/src/components/OrderComponent.tsx
+++ packages/frontend/src/components/OrderComponent.tsx
@@ -22,6 +22,27 @@ export default function OrderComponent() {
   return (
     <>
       <h1 className="text-center">Your orders:</h1>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Delivery address</th>
+            <th>Total</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders &&
+            orders.map((order, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{order.deliveryAddress}</td>
+                <td>{order.totalCost} kr</td>
+                <td>{order.status}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
     </>
   )
 }
