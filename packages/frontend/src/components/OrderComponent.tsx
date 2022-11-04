8f7231b4ba817caa3cb26aa267970293c5137d88
HannaTylna <hanna.tylna@gmail.com>
2022-11-04 09:56:13 +0100

    change fetch info

--- packages/frontend/src/components/OrderComponent.tsx
+++ packages/frontend/src/components/OrderComponent.tsx
@@ -10,25 +10,26 @@ export default function OrderComponent() {
   const getOrders = async () => {
     try {
       const response = await axios.get("/api/orders")
-      console.log(response.data)
       setOrders(response.data)
     } catch (error) {
       console.log(error)
     }
   }
+
   useEffect(() => {
     getOrders()
   }, [])
   return (
     <>
+      <hr />
       <h1 className="text-center">Your orders:</h1>
       <Table striped>
         <thead>
           <tr>
             <th>#</th>
-            <th>Products</th>
-            <th>Total</th>
-            <th>Status</th>
+            <th className="text-center">Delivery address</th>
+            <th className="text-center">Total</th>
+            <th className="text-center">Status</th>
           </tr>
         </thead>
         <tbody>
@@ -36,19 +37,11 @@ export default function OrderComponent() {
             orders.map((order, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>
-                  <ul>
-                    {order.products &&
-                      order.products.map((item, index) => (
-                        <li key={index}>
-                          {item.productId} - {item.price} kr ({item.quantity}{" "}
-                          st)
-                        </li>
-                      ))}
-                  </ul>
+                <td className="text-center">
+                  {order.deliveryAddress ? order.deliveryAddress : "-"}
                 </td>
-                <td>{order.totalCost} kr</td>
-                <td>{order.status}</td>
+                <td className="text-center">{order.totalCost} kr</td>
+                <td className="text-center">{order.status}</td>
               </tr>
             ))}
         </tbody>
