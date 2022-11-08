20d789bc0fc6ff42e8f646651542bddae49fa77b
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-08 20:48:51 +0100

    add a new route for Create operation

--- packages/backend/src/routers/admin.ts
+++ packages/backend/src/routers/admin.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express"
 import { adminAuth } from "../middleware/auth"
 import { registeredOrders } from "../controllers/order"
+import { addProduct } from "../controllers/products.controller"
 
 const adminRouter: Router = express.Router()
-adminRouter.use("/getallorders", adminAuth)
+adminRouter.use("/", adminAuth)
 adminRouter.get("/getallorders", registeredOrders)
+adminRouter.post("/add", addProduct)
 
 export default adminRouter
