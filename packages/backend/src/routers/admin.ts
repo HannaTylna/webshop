ef28cc5df80e35062090bb961fc74f59d83b8dd2
HannaTylna <hanna.tylna@gmail.com>
2022-11-08 22:22:03 +0100

    add route to update status

--- packages/backend/src/routers/admin.ts
+++ packages/backend/src/routers/admin.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express"
 import { adminAuth } from "../middleware/auth"
-import { registeredOrders } from "../controllers/order"
+import { registeredOrders, updateOrderStatus } from "../controllers/order"
 
 const adminRouter: Router = express.Router()
 adminRouter.use("/getallorders", adminAuth)
 adminRouter.get("/getallorders", registeredOrders)
+adminRouter.use("/update", adminAuth)
+adminRouter.patch("/update/:id", updateOrderStatus)
 
 export default adminRouter
