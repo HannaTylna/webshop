35742ad3ef7a5354277feacaf0b7ea96a3cbf22b
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-08 21:38:02 +0100

    add a new route for update

--- packages/backend/src/routers/admin.ts
+++ packages/backend/src/routers/admin.ts
@@ -2,10 +2,12 @@ import express, { Router } from "express"
 import { adminAuth } from "../middleware/auth"
 import { registeredOrders } from "../controllers/order"
 import { addProduct } from "../controllers/products.controller"
+import { updateProduct } from "../controllers/products.controller"
 
 const adminRouter: Router = express.Router()
 adminRouter.use("/", adminAuth)
 adminRouter.get("/getallorders", registeredOrders)
 adminRouter.post("/add", addProduct)
+adminRouter.patch("/update/:id", updateProduct)
 
 export default adminRouter
