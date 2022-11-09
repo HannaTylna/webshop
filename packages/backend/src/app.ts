6e17f3767f6840d43ca2be70dd85e24eccbee46d
HannaTylna <hanna.tylna@gmail.com>
2022-11-09 10:01:41 +0100

    add multer

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, json } from "express"
 import { setUpMongoDb } from "./models/common"
 import router from "./routers/index"
 import dotenv from "dotenv"
+import multer from "multer"
 
 dotenv.config()
 
@@ -12,6 +13,17 @@ app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || "4000")
 const mongoUrl: string =
   process.env.MONGODB_URL || "mongodb://localhost:27017/webshop"
+
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage })
+app.use(upload.array("files"))
+app.use("/api/uploads", express.static("./uploads"))
 
 app.use("/api", router)
 
