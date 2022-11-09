59e1d6ca8463f549bf23e0bd3f119c9282f31827
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-09 18:45:46 +0100

    reconfig admin.ts for multer

--- packages/backend/src/routers/admin.ts
+++ packages/backend/src/routers/admin.ts
@@ -3,11 +3,13 @@ import { adminAuth } from "../middleware/auth"
 import { registeredOrders } from "../controllers/order"
 import { addProduct } from "../controllers/products.controller"
 import { updateProduct } from "../controllers/products.controller"
+import upload from "../middleware/upload"
+
 
 const adminRouter: Router = express.Router()
-adminRouter.use("/", adminAuth)
+adminRouter.use("/", adminAuth, express.static("./uploads"))
 adminRouter.get("/getallorders", registeredOrders)
-adminRouter.post("/add", addProduct)
+adminRouter.post("/add", upload.single('images') , addProduct)
 adminRouter.patch("/update/:id", updateProduct)
 
 export default adminRouter
