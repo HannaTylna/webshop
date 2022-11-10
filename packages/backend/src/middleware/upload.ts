d5c0016e6e8603cd5eb30592d0310c9cce231c66
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-10 09:46:12 +0100

    change upload middleware

--- packages/backend/src/middleware/upload.ts
+++ packages/backend/src/middleware/upload.ts
@@ -1,10 +1,16 @@
 import multer from 'multer'
+import path from 'path'
 
 const upload = multer ({
     storage: multer.diskStorage({
-        destination: "uploads",
+        destination: function (req, file, cb) {
+          cb(null, "uploads");
+        },
         filename: function (req, file, cb) {
-          cb(null, file.originalname)
+          cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+          );
         },
     })
 })
