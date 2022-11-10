5a7ad708fb7ccce69769e1f78afa87899292e7e4
HannaTylna <hanna.tylna@gmail.com>
2022-11-10 19:57:04 +0100

    clean up

--- packages/frontend/src/pages/OrdersPage.tsx
+++ packages/frontend/src/pages/OrdersPage.tsx
@@ -1,15 +1,11 @@
 import axios from "axios"
 import Table from "react-bootstrap/Table"
 import { Order } from "@webshop/shared"
-
 import { useEffect, useState } from "react"
-import { Button, Form } from "react-bootstrap"
-import { useParams } from "react-router-dom"
+import { Button } from "react-bootstrap"
 
 export default function OrderComponent() {
   const [orders, setOrders] = useState<Order[]>([])
-  const [updatedStatus, setUpdatedStatus] = useState<string>("")
-  const id = useParams()
 
   const getOrders = async () => {
     try {
@@ -20,21 +16,10 @@ export default function OrderComponent() {
     }
   }
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault()
-    try {
-      await axios.patch(`/api/admin/info${id}`)
-      console.log(id)
-    } catch (error) {
-      console.log("Something went wrong!")
-    }
-    window.location.reload()
-  }
   useEffect(() => {
     getOrders()
   }, [])
+
   return (
     <>
       <hr />
@@ -42,31 +27,20 @@ export default function OrderComponent() {
       <Table striped>
         <thead>
           <tr>
+            <th className="text-center">#</th>
             <th className="text-center">Order id</th>
             <th className="text-center">Status</th>
-            <th className="text-center">New status</th>
-            <th className="text-center"></th>
           </tr>
         </thead>
         <tbody>
           {orders &&
             orders.map((order, index) => (
               <tr key={index}>
+                <td className="text-center">{index + 1}</td>
                 <td className="text-center">{order._id}</td>
                 <td className="text-center">{order.status}</td>
-                <td>
-                  <Form.Select aria-label="Default select example">
-                    <option>Change status</option>
-                    <option value="registered">Registered</option>
-                    <option value="processing">Processing</option>
-                    <option value="in delivery">In delivery</option>
-                    <option value="delivered">Delivered</option>
-                  </Form.Select>
-                </td>
-                <td>
-                  <Button variant="secondary" size="sm">
-                    Save
-                  </Button>
+                <td className="text-center">
+                  <Button href={`/order/${order._id}`}>More ...</Button>
                 </td>
               </tr>
             ))}
