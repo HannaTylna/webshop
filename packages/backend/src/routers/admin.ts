7b0263af16924cdd98ed5236f51a676d502b1d6b
HannaTylna <hanna.tylna@gmail.com>
2022-11-10 17:22:40 +0100

    add get order route

--- packages/backend/src/routers/admin.ts
+++ packages/backend/src/routers/admin.ts
@@ -1,17 +1,21 @@
 import express, { Router } from "express"
 import { adminAuth } from "../middleware/auth"
-import { registeredOrders, updateOrderStatus } from "../controllers/order"
+import {
+  getOrder,
+  registeredOrders,
+  updateOrderStatus,
+} from "../controllers/order"
 import { addProduct } from "../controllers/products.controller"
 import { updateProduct } from "../controllers/products.controller"
 import upload from "../middleware/upload"
 
-
 const adminRouter: Router = express.Router()
 adminRouter.use("/", adminAuth, express.static("./uploads"))
 adminRouter.get("/getallorders", registeredOrders)
-adminRouter.post("/add", upload.single('images') , addProduct)
+adminRouter.post("/add", upload.single("images"), addProduct)
 adminRouter.patch("/update/:id", updateProduct)
 adminRouter.use("/update", adminAuth)
+adminRouter.get("/update/:id", getOrder)
 adminRouter.patch("/update/:id", updateOrderStatus)
 
 export default adminRouter
